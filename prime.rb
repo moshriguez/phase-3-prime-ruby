b3d1760e33f56f0f893314bb1304dc8d06a78d2a
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-17 16:39:43 -0700

    completed

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+    if num < 2
+        return false
+    end
+    arr = (2..(Math.sqrt(num).floor)).to_a
+    arr.each do |possible_factor|
+        if num % possible_factor == 0
+            return false
+        end
+    end
+    return true
+end
+
+puts prime?(77777)
